279592c0fd5c92d23720b0f6da1b2cb1144f467f
Nick Troccoli <nicktroccoli28@gmail.com>
2016-11-17 15:54:15 -0800

    Split graph generation and filereading into 2 separate files

---
+++ GraphConstants.py
@@ -0,0 +1,12 @@
+# ------ CONSTANTS -------
+
+# NodeType: the different values a node's "type" field can have in the graph.
+NodeTypeActor = "ACTOR"
+NodeTypeDirector = "DIRECTOR"
+NodeTypeMovie = "MOVIE"
+NodeTypeActorDirector = "ACTOR-DIRECTOR"
+
+# Filenames
+datasetFilename = "movie_metadata.csv"
+graphFilename = "graph.gpickle"
+graphDictFilename = "graphdict.csv"
