3a6426d6ac2d0d5c382e60e9ec3e392b3a294b2f
Catherine Dong <cdong@cs.stanford.edu>
2016-12-09 17:50:56 -0800

    revert GenerateMovieGraph.py and update getMoreActors

---
+++ utils.py
@@ -0,0 +1,52 @@
+from lxml import html
+import requests
+
+def getRaceAndGender(name):
+	name = name.split()
+	query = '+'.join(name)
+	searchPage = requests.get('http://search.nndb.com/search/?type=unspecified&query=' + query)
+	searchTree = html.fromstring(searchPage.content)
+	if len(name) > 1:
+		condition = '[contains(.//text(), "' + name[0] + '") and contains(.//text(), "' + name[1] + '")]'
+	else:
+		condition = '[contains(.//text(), "' + name[0] + '")]'
+	personList = searchTree.xpath('//table')[3].xpath('./tr')[1].xpath('./td' + condition)
+	if personList:
+		personUrl = personList[0].xpath('.//a/@href')[0]
+	else:
+		return None
+	personPage = requests.get(personUrl)
+	personTree = html.fromstring(personPage.content)
+	race = personTree.xpath('//p/b[text()="Race or Ethnicity:"]/following-sibling::text()')[0].strip()
+	gender = personTree.xpath('//p/b[text()="Gender:"]/following-sibling::text()')[0].strip()
+	return race, gender
+
+
+from imdb import IMDb
+
+def getMovieInfo(imdbUrl):
+	imdb = IMDb()
+	movieId = imdbUrl.split('/')[4][2:]
+	movie = imdb.get_movie(movieId)
+	info = {}
+	info['actors'] = [actor['name'] for actor in movie['cast']]
+	info['production company'] = movie['production company'][0]['name']
+	info['director'] = movie['director'][0]['name']
+	info['year'] = movie['year']
+	return info
+
+def getMoreActors(imdbUrl, topActors):
+	newActors = []
+	try:
+		imdb = IMDb()
+		movieId = imdbUrl.split('/')[4][2:]
+		movie = imdb.get_movie(movieId)
+		print movie['cast']
+		for actor in movie['cast'][:6]:
+			if len(newActors) >= 3:
+				continue
+			if actor['name'] not in topActors:
+				newActors.append(actor['name'])
+	except:
+		return topActors
+	return newActors
