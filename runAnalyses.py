477e7279e8c833e55839424c37d4ae55f80ed952
Catherine Dong <cdong@cs.stanford.edu>
2016-12-10 16:57:34 -0800

    starting to fill in runAnalysis

--- runAnalyses.py
+++ runAnalyses.py
@@ -3,3 +3,18 @@ import Analysis as ana
 import DiversityScore as ds
 
 graph, graphDict = ReadMovieGraph.readMovieGraphFromFile()
+graph = ana.getGraphWithoutNones(graph)
+
+# Statistics for the real network
+actorStats = ds.actorStats(graph)
+movieStats = ds.movieStats(graph)
+directorStats = ds.directorStats(graph)
+raceModularity, blackWhiteModularity, genderModularity = ana.actorModularity(graph)
+raceAssorativity, blackWhiteAssorativity, genderAssorativity = ana.actorAssortativity(graph)
+
+
+# Movie and actor statistics for the movie-actor null model
+movieActorNullModel = ana.movieActorNullModel(graph)
+movieStats = ds.movieStats(movieActorNullModel)
+raceModularityBaseline, blackWhiteModularityBaseline, genderModularityBaseline = ana.actorModularity(graph)
+raceAssorativityBaseline, blackWhiteAssorativityBaseline, genderAssorativityBaseline = ana.actorAssortativity(graph)
